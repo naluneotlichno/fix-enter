5aa3076d5fa7af4f38cb2822cb44f06e80622aaf
naluneotlichno@yandex.ru <naluneotlichno@yandex.ru>
2025-05-25 15:05:14 +0500

    Добавил иконку

--- src/background/background.js
+++
@@ -1,8 +0,0 @@
-// Инициализация расширения
-chrome.action.onClicked.addListener(() => {
-  console.log("Smart Enter Handler activated! 🚀");
-});
-
-chrome.tabs.onActivated.addListener(function() {
-  chrome.action.setBadgeText({ text: "ON" });
-});
